e046752bc2787a5564eefd2fcb3fb7ae847ea241
ILIAS0519 <pkn8hr6br6@icloud.com>
2023-03-03 21:03:53 +0900

    階上関数の実装

--- app.js
+++ app.js
@@ -5,3 +5,11 @@ for (let i = 1; i <= number; i++) {
     sum = sum + i;
 }
 console.log(sum);
+function factorial(n) {
+    let result = 1;
+    //TO DO このコメントを消して正しく実装してください。
+    for (let i = 1; i <= n; i++) {
+        result = result * i;
+    }
+    return result
+}
