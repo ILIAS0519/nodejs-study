cd5ec08aae036081b6eb0f6ae6ff0a6b2e12a146
ILIAS0519 <pkn8hr6br6@icloud.com>
2023-03-05 16:53:24 +0900

    first commit

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
-'use strict'
+'use strict';
 const number = process.argv[2] || 0;
 let sum = 0;
 for (let i = 1; i <= number; i++) {
-    sum = sum + i;
+  sum = sum + i;
 }
 console.log(sum);
-function factorial(n) {
-    let result = 1;
-    //TO DO このコメントを消して正しく実装してください。
-    for (let i = 1; i <= n; i++) {
-        result = result * i;
-    }
-    return result
-}
